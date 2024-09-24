7fa828f5861c808be68127b0ffcb8393aa5ab43a
Derek Gusoff <dgusoff@rightpoint.com>
2024-09-24 19:45:35 -0400

    init

---
+++ Azure.OpenAI.ChatCompletions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure.OpenAI.ChatCompletions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
